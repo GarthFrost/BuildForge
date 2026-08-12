63e206483a4f35835352b32280c3fcd73470edde
GarthFrost <frostlightmesser@gmail.com>
2026-08-11 22:57:09 -0400

    Add header image import in App component
    Added header image import for the application.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,8 @@ import Starfieldsplash from "./components/Starfieldsplash";
 import PowerCurveGraph from "./components/PowerCurveGraph";
 import { useBuildForge } from "./hooks/useBuildForge";
 import RuneCarousel from "./components/RuneCarousel";
-import Bfheader from "./components/Bfheader";   // ← NEW HEADER IMPORT
+import headerImg from "../assets/transbfheader0.PNG";
+   // ← NEW HEADER IMPORT
 
 
 export default function App() {
