81ad7dbe1400ff2ddb45265d8090512a1cd1fa12
GarthFrost <frostlightmesser@gmail.com>
2026-08-11 23:14:45 -0400

    Update header image file name in Bfheader component

--- frontend/src/components/Bfheader.jsx
+++ frontend/src/components/Bfheader.jsx
@@ -1,6 +1,6 @@
 // frontend/src/components/Bfheader.jsx
 import React from "react";
-import headerImg from "../assets/transbfheader0.PNG";
+import headerImg from "../assets/transbfheader.png";
 
 export default function Bfheader() {
   return (
